fd6ab1738e42a785d159619a1778b974940e942f
JustinP <booooo201@gmail.com>
2013-06-17 13:59:13 -0400

    Worked on backend stuff..

--- CrossOver/Form1.Designer.cs
+++ CrossOver/Form1.Designer.cs
@@ -73,6 +73,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "CrossOver - Upload";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- CrossOver/Form1.cs
+++ CrossOver/Form1.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using CrossOver.MethodResult;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,9 +16,79 @@ namespace CrossOver
 {
     public partial class Form1 : Form
     {
+        Computer[] allComputers;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FindComputers();
+        }
+
+        private void FindComputers()
+        {
+            try
+            {
+                //Find prefix
+                string localIP = LocalIPAddress();
+                string[] values = localIP.Split('.');
+                string prefix = values[0] + "." + values[1];
+
+                Computer[] ips = ScanComputers(prefix);
+                allComputers = ips;
+                foreach (Computer ip in ips)
+                {
+                    //Check if the port is open
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error while getting local computers. Error: " + e.Message, "Cross Over - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public Computer[] ScanComputers(string ipPrefix)
+        {
+            List<Computer> computers = new List<Computer>();
+            for (int i = 0; i <= 255; i++)
+            {
+                string scanIP = ipPrefix + "." + i.ToString();
+                IPAddress myScanIP = IPAddress.Parse(scanIP);
+                IPHostEntry myScanHost = null;
+                try
+                {
+                    myScanHost = Dns.GetHostEntry(myScanIP);
+                }
+                catch
+                {
+                    continue;
+                }
+                if (myScanHost != null)
+                {
+                    computers.Add(new Computer(myScanHost.HostName.ToString(), myScanIP.ToString()));
+                }
+            }
+
+            return computers.ToArray();
+        }
+
+        private string LocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
         }
     }
 }
